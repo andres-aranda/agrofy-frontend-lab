95df40575fddcebeb1c3abeafa8f78b028221966
andres-aranda <61162316+andres-aranda@users.noreply.github.com>
2020-03-12 11:19:44 -0300

    m

--- week3-4/src/App.js
+++ week3-4/src/App.js
@@ -8,16 +8,17 @@ const Home = lazy(() => import('./pages/Home/Home'));
 const Pokedex = lazy(() => import('./pages/Pokedex/Pokedex'));
 const Login = lazy(()=>import('./pages/Login/Login'));
 const Contact = lazy(()=>import('./components/Contact/Contact'));
-let pokemones = [];
-//let fav = JSON.parse(localStorage.getItem("fav")) || [];
-//Lamada a la API
+
+/*{<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style={{margin:'auto',background:'rgb(255, 255, 255)', display:'block', shapeRendering:'auto'}} width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+<path fill="none" stroke="#e90c59" strokeWidth="8" strokeDasharray="42.76482137044271 42.76482137044271" d="M24.3 30C11.4 30 5 43.3 5 50s6.4 20 19.3 20c19.3 0 32.1-40 51.4-40 C88.6 30 95 43.3 95 50s-6.4 20-19.3 20C56.4 70 43.6 30 24.3 30z" strokeLinecap="round" style="transform:scale(0.8);transform-origin:50px 50px">
+  <animate attributeName="stroke-dashoffset" repeatCount="indefinite" dur="1s" keyTimes="0;1" values="0;256.58892822265625"></animate></path></svg>}*/
 
 const App = () => {
   return (
   <React.Fragment>
     <Router>
       <Header />
-      <Suspense fallback={<div>loading...</div>}>
+      <Suspense fallback={<div>loading</div>}>
         <Switch>     
           <Route exact path="/" component={Home} />}/>
           <Route exact path="/pokedex" component={Pokedex} />
--- week3-4/src/components/FormLogin/FormLogin.js
+++ week3-4/src/components/FormLogin/FormLogin.js
@@ -1,17 +1,39 @@
 import React, { useState } from 'react';
 import './FormLogin.css';
 
+const FormLogin =()=>{
+const [mail, setMail] = useState('');
+const [pass, setPass] = useState('');
+
+const validate = () => {
+  return mail.length && pass.length > 4;
+};
+
+function handleSubmit(event) {
+  event.preventDefault();
+}
+const myRequest = new Request('https://reqres.in/api/login',{
+  "email": "eve.holt@reqres.in",
+  "password": "cityslicka"
+});
+fetch(myRequest).then(res=>console.log('reeess', res)).catch(err=> console.log('eerror', err))
+
+const myURL = myRequest.url; // http://localhost/flowers.jpg
+const myMethod = myRequest.method; // GET
+const myCred = myRequest.credentials; // omit
 
 
-const FormLogin =()=>(
+
+
+  return(
     <div class="formulariLogin">
-    <form action="">
-      <input class="entradaLogin" type="email" name="email" size="40" placeholder="E-Mail"/><br/>
-      <input class="entradaLogin" type="password" name="password" size="40" placeholder="Password"/><br/>
+    <form onSubmit={handleSubmit}>
+      <input class="entradaLogin" type="email" name="email" size="40" placeholder="E-Mail" onChange={event => setMail(event.target.value)}/><br/>
+      <input class="entradaLogin" type="password" name="password" size="40" placeholder="Password"onChange={event => setPass(event.target.value)}/><br/>
     </form>
-    <button className='btnLogin'>Login</button>
+    <button type="submit" className='btnLogin' isDisabled={!validate()}>Login</button>
   </div>
-)
+)}
 
 
 export default FormLogin;
