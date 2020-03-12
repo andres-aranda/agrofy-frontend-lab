dd15ccb631841f0c85a903b17b7df7dc86275a95
andres-aranda <61162316+andres-aranda@users.noreply.github.com>
2020-03-12 14:25:06 -0300

    api rsponde

--- week3-4/src/components/FormLogin/FormLogin.js
+++ week3-4/src/components/FormLogin/FormLogin.js
@@ -1,35 +1,63 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './FormLogin.css';
 
-const FormLogin =()=>{
-const [mail, setMail] = useState('');
-const [pass, setPass] = useState('');
+const FormLogin = () => {
+    const [mail, setMail] = useState('');
+    const [pass, setPass] = useState('');
 
-const validate = () => {
-  return mail.length && pass.length > 4;
-};
+    function validate() {
+        return mail.length && pass.length > 4;
+    }
 
-function handleSubmit(event) {
-  event.preventDefault();
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        let defaultOptions = {
+            url: '',
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                "Content-Type": "application/json;charset=UTF-8",
+                "Authorization": localStorage.token
+            },
+            dataType: 'json',
+            body: JSON.stringify({
+                "email": mail,//"eve.holt@reqres.in",
+                "password": pass//"cityslicka"
+            }),
+        };
+        fetch('https://reqres.in/api/login', defaultOptions)
+            .then(function (response) {
+                if (response.status >= 400) {
+                    return Promise.reject(response);
+                } else {
+                    var data = response.json();
+                    return data;
+                }
+            })
+            .then(function (data) {
+                console.log(data);
+                localStorage.token = data.token;
+            })
+            .catch(function (error) {
+
+            });
+    }
+
+
+    return (
+        <div class="formulariLogin">
+            <form onSubmit={handleSubmit}>
+                <input class="entradaLogin" type="email" name="email" size="40" placeholder="E-Mail"
+                       onChange={event => setMail(event.target.value)}/><br/>
+                <input class="entradaLogin" type="password" name="password" size="40" placeholder="Password"
+                       onChange={event => setPass(event.target.value)}/><br/>
+                <button type="submit" className='btnLogin' disabled={!validate()}>Login</button>
+            </form>
+        </div>
+    )
 }
-/*
-const myRequest = new Request('https://reqres.in/api/login',{
-  "email": "eve.holt@reqres.in",
-  "password": "cityslicka"
-});
-fetch(myRequest).then(res=>console.log('reeess', res)).catch(err=> console.log('eerror', err))
-*/
-
-
-  return(
-    <div class="formulariLogin">
-    <form onSubmit={handleSubmit}>
-      <input className="entradaLogin" type="email" name="email" size="40" placeholder="E-Mail" onChange={event => setMail(event.target.value)}/><br/>
-      <input className="entradaLogin" type="password" name="password" size="40" placeholder="Password"onChange={event => setPass(event.target.value)}/><br/>
-    </form>
-    <button type="submit" className='btnLogin' disabled={!validate()}>Login</button>
-  </div>
-)}
 
 
 export default FormLogin;
